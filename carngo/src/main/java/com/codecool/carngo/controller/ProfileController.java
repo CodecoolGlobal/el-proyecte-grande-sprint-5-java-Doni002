aa34660db93bea580c7816f3db7068cdc49a6fbf
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-07-27 14:20:25 +0200

    change allProfiles type from Set to List and render it in html

--- carngo/src/main/java/com/codecool/carngo/controller/ProfileController.java
+++ carngo/src/main/java/com/codecool/carngo/controller/ProfileController.java
@@ -3,15 +3,18 @@ package com.codecool.carngo.controller;
 import com.codecool.carngo.model.ProfileModel;
 import com.codecool.carngo.service.ProfileService;
 import org.springframework.beans.factory.annotation.Autowired;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import java.util.Set;
+import java.util.List;
+
 
 @RequestMapping("/profiles")
 @Controller
@@ -24,11 +27,13 @@ public class ProfileController {
     }
 
     @GetMapping()
-    public String allProfiles(){
+    public String allProfiles(Model model){
+        List<ProfileModel> profiles = profileService.getAllProfiles();
+        model.addAttribute("profiles", profiles);
         return "profile";
     }
     @PostMapping
-    public ResponseEntity<Set<ProfileModel>> getAllFeedbacks(){
+    public ResponseEntity<List<ProfileModel>> getAllFeedbacks(){
         return new ResponseEntity<>(profileService.getAllProfiles(), HttpStatus.ACCEPTED);
     }
     @GetMapping("/{id}")
