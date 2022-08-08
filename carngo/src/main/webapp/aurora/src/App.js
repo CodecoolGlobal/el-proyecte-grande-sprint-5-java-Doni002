9f6b13b7dfa2faeea1750bc22e929c1d84720f17
RolandMarton <roland111187@gmail.com>
2022-08-08 13:17:25 +0200

    Rework: React-router-dom use, link, route, routes

--- carngo/src/main/webapp/aurora/src/App.js
+++ carngo/src/main/webapp/aurora/src/App.js
@@ -1,20 +1,21 @@
 import './App.css';
 import Home from "./pages/Home";
 import Vehicles from "./pages/Vehicles";
+import Navbar from "./components/Navbar";
+import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 function App() {
-    let component;
-    switch (window.location.pathname) {
-        case "/":
-            component = <Home />
-            break
-        case "/vehicles":
-            component = <Vehicles />
-            break
-    }
   return (
     <>
-        <div className="container">{component}</div>
+        <Navbar />
+        <div className="container">
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element= {<Home />} />;
+                    <Route path="/vehicles" element= {<Vehicles />} />
+                </Routes>
+            </BrowserRouter>
+        </div>
     </>
   );
 }
