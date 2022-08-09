38f311b29bb8b5c2b0d211bcb1e56ade9b9e9fee
RolandMarton <roland111187@gmail.com>
2022-08-09 13:35:22 +0200

    Add: Navbar restructured

--- carngo/src/main/webapp/aurora/src/components/header/Navbar.js
+++ carngo/src/main/webapp/aurora/src/components/header/Navbar.js
@@ -6,9 +6,9 @@ const Navbar = () => {
     return (
         <>
             <nav>
+                <li className="logo"><Link to="/"><h1>Aurora</h1></Link></li>
                 <ul className='menu'>
-                    <li className="logo"><Link to="/"><h1>Aurora</h1></Link></li>
-                    <li className="item"><Link to="/shareyourcar"> Share your car </Link></li>
+                    <li className="item"><Link to="/shareyourcar"> <strong>Share your car</strong> </Link></li>
                     <li className="item"><Link to="/vehicles"> Vehicles </Link></li>
                     <li className="item"><Link to="/contact"> Contact us </Link></li>
                     <li className="item"><Link to="/profile"> Profile </Link></li>
