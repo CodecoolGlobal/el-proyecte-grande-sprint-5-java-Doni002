f94236636bc5fcf1c40bb0ce907758aed86c429c
RolandMarton <roland111187@gmail.com>
2022-08-09 13:01:11 +0200

    Add: Navbar Structure change, preparation for styling

--- carngo/src/main/webapp/aurora/src/components/header/Navbar.js
+++ carngo/src/main/webapp/aurora/src/components/header/Navbar.js
@@ -1,23 +1,21 @@
 import {Link} from "react-router-dom";
-import { Parallax } from 'react-parallax';
 
-import AudiFront from "../img/Audi Front.jpg"
-import './Navbar.css';
+import './Header.css';
 
 const Navbar = () => {
     return (
-        <Parallax className='image' bgImage={AudiFront} strength={800}>
-            <div className='navbar'>
-                <Link to="/">Aurora</Link>
-                <Link to="/shareyourcar"> <button>Share your car</button> </Link>
-                <Link to="/vehicles"> <button>Vehicles</button> </Link>
-                <Link to="/contact"> <button>Contact us</button> </Link>
-                <Link to="/profile"> <button>Profile</button> </Link>
-            </div>
-            <div className='content'>
-                <span className="img-txt">The world of <strong>Aurora</strong> is now</span>
-            </div>
-        </Parallax>
+        <>
+            <nav>
+                <ul className='menu'>
+                    <li className="logo"><Link to="/"><h1>Aurora</h1></Link></li>
+                    <li className="item"><Link to="/shareyourcar"> Share your car </Link></li>
+                    <li className="item"><Link to="/vehicles"> Vehicles </Link></li>
+                    <li className="item"><Link to="/contact"> Contact us </Link></li>
+                    <li className="item"><Link to="/profile"> Profile </Link></li>
+                </ul>
+            </nav>
+        </>
+
     );
 };
 
