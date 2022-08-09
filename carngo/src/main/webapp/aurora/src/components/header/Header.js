015bdba563634c1aea1ae59fe6adcf76a8b6f8f6
RolandMarton <roland111187@gmail.com>
2022-08-09 13:36:51 +0200

    Add: Image import add

--- carngo/src/main/webapp/aurora/src/components/header/Header.js
+++ carngo/src/main/webapp/aurora/src/components/header/Header.js
@@ -1,5 +1,7 @@
 import {Parallax} from "react-parallax";
-import AudiBack from "../img/Audi Back.jpg";
+// import AudiBack from "../img/Audi Back.jpg";
+import AudiFront from "../img/Audi Front.jpg";
+
 import Navbar from "./Navbar";
 import Content from "./Content";
 
@@ -7,7 +9,7 @@ import Content from "./Content";
 const Header = () => {
     return (
         <>
-            <Parallax className='image' bgImage={AudiBack} strength={800}>
+            <Parallax className='image' bgImage={AudiFront} strength={800}>
                 <Navbar />
                 <Content />
             </Parallax>
