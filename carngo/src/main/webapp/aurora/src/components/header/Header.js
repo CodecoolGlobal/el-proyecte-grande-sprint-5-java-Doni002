39183c0cd58a40bdad3053d5c758d2ea488255ba
RolandMarton <roland111187@gmail.com>
2022-08-09 13:05:59 +0200

    Rework: Header parrallax effect restructured

--- carngo/src/main/webapp/aurora/src/components/header/Header.js
+++ carngo/src/main/webapp/aurora/src/components/header/Header.js
@@ -1,9 +1,16 @@
+import {Parallax} from "react-parallax";
+import AudiBack from "../img/Audi Back.jpg";
 import Navbar from "./Navbar";
+import Content from "./Content";
+
 
 const Header = () => {
     return (
         <>
-            <Navbar />
+            <Parallax className='image' bgImage={AudiBack} strength={800}>
+                <Navbar />
+                <Content />
+            </Parallax>
         </>
     );
 };
