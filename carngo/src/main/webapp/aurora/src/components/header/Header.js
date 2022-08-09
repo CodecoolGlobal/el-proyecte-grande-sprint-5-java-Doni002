7cd874bd081fb9f3c61f63810f2e6de7f1acc4e2
RolandMarton <roland111187@gmail.com>
2022-08-09 15:07:44 +0200

    Add: Changing background for header

--- carngo/src/main/webapp/aurora/src/components/header/Header.js
+++ carngo/src/main/webapp/aurora/src/components/header/Header.js
@@ -1,15 +1,35 @@
 import {Parallax} from "react-parallax";
-// import AudiBack from "../img/Audi Back.jpg";
+import AudiBack from "../img/Audi Back.jpg";
 import AudiFront from "../img/Audi Front.jpg";
+import AudiWater from "../img/audiwater.jpg"
+import SteeringWheelAudi from "../img/steeringwheelaudi.jpg"
 
 import Navbar from "./Navbar";
 import Content from "./Content";
+import {useEffect, useState} from "react";
 
+const images = [
+    AudiFront,
+    AudiBack,
+    SteeringWheelAudi,
+    AudiWater
+];
 
 const Header = () => {
+    const [index, setIndex] = useState(0)
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setIndex((v) => {
+                return v === images.length - 1 ? 0 : v + 1;
+            });
+        }, 4000);
+        return () => clearInterval(interval);
+    }, []);
+
     return (
         <>
-            <Parallax className='image' bgImage={AudiFront} strength={800}>
+            <Parallax className='image' bgImage={images[index]} strength={800}>
                 <Navbar />
                 <Content />
             </Parallax>
