a1648d2b289d9247b23625dc2800fd37da90dd83
RolandMarton <roland111187@gmail.com>
2022-08-10 15:23:29 +0200

    Rework: Main page reorganised, restructured

--- carngo/src/main/webapp/aurora/src/pages/Home.js
+++ carngo/src/main/webapp/aurora/src/pages/Home.js
@@ -3,7 +3,6 @@ import Footer from '../components/footer/Footer';
 import Header from '../components/header/Header';
 import MapGuy from '../components/img/Guywithamap.jpg'
 import Business from '../components/img/business.jpg'
-import Money from '../components/img/Money.jpg'
 
 import CategorySlider from "../components/category/CategorySlider";
 import ToggleArticle from '../components/toggleArticle/ToggleArticle';
@@ -12,7 +11,6 @@ const Home = () => {
     return (
         <>          
           <Header />
-          <ToggleArticle />
             <Article
               text = "There are more than 1 million hosts worldwide to get your car from, and
 waiting for your reply to start your journey. The choice is yours to seize
@@ -27,7 +25,7 @@ friends or family with your dream car, with the help of Aurora."
             <Article
               text = "Aurora gives you the freedom to become a host for people in need of
 transportation, and joy. If you have the opportunity to share your car and
-get e x clusive rewards with percentage-based passive income. Don't wait no
+get exclusive rewards with percentage-based passive income. Don't wait no
 more. This is the right place to get the business done with our help."
               title = "Become a host"
               imageSource = {Business}
@@ -35,14 +33,7 @@ more. This is the right place to get the business done with our help."
               link = "/vehicles"
               pictureSide = "right"
               />
-            <Article
-              text = "At Article, we’re all about great style. Think sleek modular sofas, anything-but-basic beds, smart sideboards, and more — all designed with premium quality and comfort in mind. It’s contemporary furniture meets unparalleled value. Nice, right?"
-              title = "hello im title"
-              imageSource = {Money}
-              buttonText = "Get started"
-              link = "/vehicles"
-              pictureSide = "left"
-              />
+            <ToggleArticle />
             <CategorySlider/>
             <Footer />
         </>
