84918b397e72cdcc7d16b2caf551947c0b47ac3b
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-08-10 14:13:44 +0200

    Customize progressbar

--- carngo/src/main/webapp/aurora/src/components/statistics/ProgressBarContainer.js
+++ carngo/src/main/webapp/aurora/src/components/statistics/ProgressBarContainer.js
@@ -1,44 +1,40 @@
-import React, {useState} from 'react';
+import React from 'react';
 import ProgressBar from "./ProgressBar";
 import "./statistics.css"
 
-function ProgressBarContainer(props) {
-    /*const [count, setCount] = useState(0);
-    const [color, setColor] = useState("");
-    const [text, setText] = useState("");*/
-
+function ProgressBarContainer() {
     const customerCount = 300;
-     const customerColor = `hsl(353, 100%, 50%)`;
+     const customerColor = `hsl(37, 100%, 50%)`;
     const customerText = "MILLION CUSTOMER"
     const vehicleCount = 100;
-    const vehicleColor = "hsl(241, 100%, 50%)";
+    const vehicleColor = "hsl(18,100%,50%)";
     const vehicleText = "MILLION VEHICLE"
     const countryCount = 85;
-    const countryColor = "hsl(89, 100%, 29%)";
+    const countryColor = "hsl(0,85%,29%)";
     const countryText = "COUNTRY"
     const journeyCount = 150;
-    const journeyColor = "hsl(52, 100%, 47%)";
+    const journeyColor = "hsl(18,63%,52%)";
     const journeyText = "MILLION JOURNEY"
 
     return (
         <section className={"statistics-container"}>
                 <ProgressBar
-                value={customerCount}
+                count={customerCount}
                 color={customerColor}
                 type = {customerText}
                 />
                 <ProgressBar
-                value={vehicleCount}
+                count={vehicleCount}
                 color={vehicleColor}
                 type = {vehicleText}
                 />
                 <ProgressBar
-                value={countryCount}
+                count={countryCount}
                 color={countryColor}
                 type = {countryText}
                 />
                 <ProgressBar
-                value={journeyCount}
+                count={journeyCount}
                 color={journeyColor}
                 type = {journeyText}
                 />
