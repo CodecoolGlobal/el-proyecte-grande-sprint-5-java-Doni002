45b078af3d7659a66b5ae7846e035802aeec61e1
RolandMarton <roland111187@gmail.com>
2022-08-10 16:54:13 +0200

    Rework: Making statistics responsive and fit for the website

--- carngo/src/main/webapp/aurora/src/components/statistics/ProgressBarContainer.js
+++ carngo/src/main/webapp/aurora/src/components/statistics/ProgressBarContainer.js
@@ -5,13 +5,13 @@ import "./statistics.css"
 function ProgressBarContainer() {
     const customerCount = 300;
      const customerColor = `hsl(37, 100%, 50%)`;
-    const customerText = "MILLION CUSTOMER"
+    const customerText = "MILLION CUSTOMERS"
     const vehicleCount = 100;
     const vehicleColor = "hsl(18,100%,50%)";
-    const vehicleText = "MILLION VEHICLE"
+    const vehicleText = "MILLION VEHICLES"
     const countryCount = 85;
     const countryColor = "hsl(0,85%,29%)";
-    const countryText = "COUNTRY"
+    const countryText = "COUNTRIES"
     const journeyCount = 150;
     const journeyColor = "hsl(18,63%,52%)";
     const journeyText = "MILLION JOURNEY"
