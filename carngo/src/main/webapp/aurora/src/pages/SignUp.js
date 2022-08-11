6314e2076743d834bdce2f0ae637876c5a322b14
doni002 <alpardonat02@gmail.com>
2022-08-11 15:13:01 +0200

    Add Login modal

---
+++ carngo/src/main/webapp/aurora/src/pages/SignUp.js
@@ -0,0 +1,13 @@
+import Home from "./Home";
+import SignUpWindow from "../components/signUp/SignUpWindow";
+
+const SignUp = () => {
+    return (
+        <div>
+            <Home />
+            <SignUpWindow />
+        </div>
+    );
+}
+
+export default SignUp;
