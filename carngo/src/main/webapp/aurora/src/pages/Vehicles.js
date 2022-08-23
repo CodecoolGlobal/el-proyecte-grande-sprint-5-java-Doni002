df42b689cf9ba4160ff52cb066a136e4bd09f397
RolandMarton <roland111187@gmail.com>
2022-08-23 12:07:31 +0200

    Hotfix: Remove unneccesary components

--- carngo/src/main/webapp/aurora/src/pages/Vehicles.js
+++ carngo/src/main/webapp/aurora/src/pages/Vehicles.js
@@ -3,7 +3,6 @@ import DisplayMap from "../components/mapComponent/DisplayMap";
 import Navbar from "../components/header/Navbar";
 import {Parallax} from "react-parallax";
 import CountryRoad from "../components/img/countryroad.jpg"
-import Vehicle from "../components/vehicle/Vehicle";
 import CarSlider from "../components/car/CarSlider";
 import Footer from "../components/footer/Footer";
 
@@ -15,7 +14,6 @@ const Vehicles = () => {
                 <DisplayMap />
             </Parallax>
             <CarSlider/>
-            <Vehicle />
             <Footer />
         </>
     );
