6c709f00c7277117bad10f8b5969dcfd6b0b73bf
RolandMarton <roland111187@gmail.com>
2022-08-23 22:24:16 +0200

    Add: Contact route add

--- carngo/src/main/webapp/aurora/src/components/contact/contactContainer.js
+++ carngo/src/main/webapp/aurora/src/components/contact/contactContainer.js
@@ -5,12 +5,28 @@ const ContactContainer = (props) => {
 
     const contactInfos = [
         {
-            textType : "Email",
-            textValue : "aurora@carngo.com"
+            emailType : "Email",
+            emailValue : "roland111187@gmail.com.com",
+            phoneType : "Phone",
+            phoneValue : "+3630945128"
         },
         {
-            textType : "Phone",
-            textValue : "+36309504060"
+            emailType : "Email",
+            emailValue : "szabo.huszar.barbara@gmail.com",
+            phoneType : "Phone",
+            phoneValue : "+36301234567"
+        },
+        {
+            emailType : "Email",
+            emailValue : "alpardonat02@gmail.com",
+            phoneType : "Phone",
+            phoneValue : "+36301234567"
+        },
+        {
+            emailType : "Email",
+            emailValue : "zsol.pz@gmail.com ",
+            phoneType : "Phone",
+            phoneValue : "+36301234567"
         }
     ]
 
@@ -23,20 +39,27 @@ const ContactContainer = (props) => {
         }
     )
     return (
-        <div className="content">
+        <div className="contactUsContainer">
             <div className="contactInfo">
                 <ul>
-                    <li><p>{contactInfos[0].textType}</p></li>
-                    <li><p>{contactInfos[0].textValue}</p></li>
+                    <li><p>{contactInfos[0].emailType}</p></li>
+                    <li><p>{contactInfos[0].emailValue}</p></li>
+                    <li><p>{contactInfos[1].emailValue}</p></li>
+                    <li><p>{contactInfos[2].emailValue}</p></li>
+                    <li><p>{contactInfos[3].emailValue}</p></li>
                 </ul>
                 <ul>
-                    <li><p>{contactInfos[1].textType}</p></li>
-                    <li><p>{contactInfos[1].textValue}</p></li>
+
+                    <li><p>{contactInfos[0].phoneType}</p></li>
+                    <li><p>{contactInfos[0].phoneValue}</p></li>
+                    <li><p>{contactInfos[1].phoneValue}</p></li>
+                    <li><p>{contactInfos[2].phoneValue}</p></li>
+                    <li><p>{contactInfos[3].phoneValue}</p></li>
                 </ul>
-                <div className="contactUsContainer">
-                    <h1>{header}</h1>
-                    <p>{text}</p>
-                </div>
+            </div>
+            <div className="contactUsText">
+                <h1>{header}</h1>
+                <p>{text}</p>
             </div>
         </div>
     );
