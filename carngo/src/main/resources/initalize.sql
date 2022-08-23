d3cb655da2891b45c24d9d2144f735e952cef3d6
doni002 <alpardonat02@gmail.com>
2022-08-23 11:50:02 +0200

    create database initalize.sql

--- carngo/src/main/resources/initalize.sql
+++ carngo/src/main/resources/initalize.sql
@@ -1,18 +1,93 @@
+DROP TABLE IF EXISTS car_availability;
+DROP TABLE IF EXISTS car_feedback;
+DROP TABLE IF EXISTS user_feedback;
+DROP TABLE IF EXISTS car_reservation;
 DROP TABLE IF EXISTS vehicles;
+DROP TABLE IF EXISTS hosts;
+DROP TABLE IF EXISTS users;
+
+
+
+
+CREATE TABLE users (
+    id int,
+    name varchar(255),
+    email varchar(255),
+    password varchar(255),
+    PRIMARY KEY (id)
+);
+
+CREATE TABLE hosts (
+    id int,
+    bank_account_number int,
+    bank_account_name varchar(255),
+    user_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (user_id)
+    REFERENCES users(id)
+);
+
 
 CREATE TABLE vehicles (
-                         id int,
-                         description varchar(255),
-                         car_type varchar(255),
-                         color varchar(255),
-                         brand varchar(255),
-                         model varchar(255),
-                         fuel varchar(255),
-                         vintage int,
-                         num_of_seats int,
-                         trunk_capacity int,
-                         price_per_day int,
-                         num_of_reservations int,
-                         owner_id int,
-                         PRIMARY KEY (id)
+     id int,
+     description varchar(255),
+     car_type varchar(255),
+     color varchar(255),
+     brand varchar(255),
+     model varchar(255),
+     fuel varchar(255),
+     vintage int,
+     num_of_seats int,
+     trunk_capacity int,
+     price_per_day int,
+     num_of_reservations int,
+     owner_id int,
+     PRIMARY KEY (id),
+     FOREIGN KEY (owner_id)
+     REFERENCES hosts(id)
 );
+
+CREATE TABLE car_availability (
+    car_id int,
+    "from" int,
+    "to" int,
+    status boolean,
+    FOREIGN KEY (car_id)
+    REFERENCES vehicles(id)
+);
+
+CREATE TABLE car_reservation (
+    id int,
+    "from" date,
+    "to" date,
+    renter_user_id int,
+    car_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (car_id)
+    REFERENCES vehicles(id),
+    FOREIGN KEY (renter_user_id)
+    REFERENCES  users(id)
+);
+
+CREATE TABLE car_feedback (
+    id int,
+    star_rating int,
+    message varchar(255),
+    reserve_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (reserve_id)
+    REFERENCES car_reservation(id)
+);
+
+CREATE TABLE user_feedback (
+    id int,
+    star_rating int,
+    message varchar(255),
+    reserve_id int,
+    PRIMARY KEY (id),
+    FOREIGN KEY (reserve_id)
+    REFERENCES car_reservation(id)
+);
+
+
+
