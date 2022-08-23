7bfef98711118b50bb5c9d033402e2bb428c3ee5
RolandMarton <roland111187@gmail.com>
2022-08-23 18:04:25 +0200

    Add: Contact Us Route add + style + properties changed

--- carngo/src/main/webapp/aurora/src/App.js
+++ carngo/src/main/webapp/aurora/src/App.js
@@ -4,6 +4,7 @@ import Vehicles from "./pages/Vehicles";
 import VehicleDetail from "./components/vehicle/VehicleDetail";
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 import Profile from './pages/Profile';
+import Contact from "./pages/Contact";
 
 function App() {
   return (
@@ -14,6 +15,7 @@ function App() {
                     <Route path="/" element= {<Home />} />;
                     <Route path="/vehicles" element= {<Vehicles />} />
                     <Route path="/profile" element= {<Profile />} />
+                    <Route path="/contact" element= {<Contact />} />
                     <Route path="/vehicles/:id" element= {<VehicleDetail />} />
                 </Routes>
             </BrowserRouter>
