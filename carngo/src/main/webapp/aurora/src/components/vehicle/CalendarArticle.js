2200c88cf44277851ff45899296ff9662bb8b3b5
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-08-24 12:57:22 +0200

    Add range to Calendar

--- carngo/src/main/webapp/aurora/src/components/vehicle/CalendarArticle.js
+++ carngo/src/main/webapp/aurora/src/components/vehicle/CalendarArticle.js
@@ -4,13 +4,26 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 
 function CalendarArticle(props) {
-    const [value, onChange] = useState(new Date());
+    const [date, setDate] = useState(new Date());
     return (
         <>
             <div className={"vArticle"}>
                 <div className={"vTextContainer"}>
                     <h1>Calendar</h1>
-                    <Calendar onChange={onChange} value={value} />
+                    <Calendar onChange={setDate} value={date} selectRange={true} />
+                    <div className="text-center">
+                        {date.length > 0 ? (
+                            <p>
+                                <span><strong>Start: </strong></span>{' '} {date[0].toDateString()}
+                                &nbsp; to &nbsp;
+                                <span><strong>End: </strong></span> {date[1].toDateString()}
+                            </p>
+                        ) : (
+                            <p>
+                                <span>Default selected date:</span>{' '} {date.toDateString()}
+                            </p>
+                        )}
+                    </div>
                 </div>
             </div>
 
