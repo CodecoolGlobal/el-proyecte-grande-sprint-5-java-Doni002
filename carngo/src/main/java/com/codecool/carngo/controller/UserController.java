95fdaec4393fc91517c9e1038d13e81bbf312c82
doni002 <alpardonat02@gmail.com>
2022-08-25 14:32:50 +0200

    add find user by id

--- carngo/src/main/java/com/codecool/carngo/controller/UserController.java
+++ carngo/src/main/java/com/codecool/carngo/controller/UserController.java
@@ -8,9 +8,11 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.List;
+import java.util.Optional;
 
 @RequestMapping("/api/users")
 @CrossOrigin(origins = "http://localhost:3000")
@@ -27,5 +29,14 @@ public class UserController {
     @GetMapping
     public ResponseEntity<List<UserModel>> getAllUser(){
         return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
+    }
+
+    @GetMapping(value = "{id}")
+    public ResponseEntity<Optional<UserModel>> getUserById(@PathVariable("id") Long id){
+        Optional<UserModel> user = userService.getUserById(id);
+        if(user.isPresent()){
+            return new ResponseEntity<>(user, HttpStatus.OK);
+        }
+        return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 }
