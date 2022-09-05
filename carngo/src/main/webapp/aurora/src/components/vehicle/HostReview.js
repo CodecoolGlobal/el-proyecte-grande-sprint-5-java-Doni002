0ff1657b40ac60cc722aae40f4d0d741ec558677
zsollti <zsol.pz@gmail.com>
2022-09-05 23:58:15 +0200

    use PofilePicture component at host review

--- carngo/src/main/webapp/aurora/src/components/vehicle/HostReview.js
+++ carngo/src/main/webapp/aurora/src/components/vehicle/HostReview.js
@@ -2,34 +2,17 @@ import "./vehicleDetail.css"
 
 import {GiRoundStar} from 'react-icons/gi';
 import HostReviewSlider from "./HostReviewSlider";
-import {useEffect, useState} from "react";
+import ProfilePicture from "../profilePicture/ProfilePicture";
 
 
 function HostReview(props) {
     const carData = props.data;
-    const [error, setError] = useState([]);
-    const [img, setImg] = useState(undefined);
-
-    useEffect(() => {
-        const fetchImage = async () => {
-            const response = await fetch(`http://localhost:8080/api/image/userProfile/${carData.host.user.imageSource}`);
-            const imageBlob = await response.blob();
-            const imageObjectURL = URL.createObjectURL(imageBlob);
-            setImg(imageObjectURL);
-        };
-
-       fetchImage().then();
-    }, [error, carData]);
-
-    if(img === undefined){
-        return (<h1>Loading...</h1>)
-    }
     return (
         <>
             <div className={"host-h3"}>HOSTED BY</div>
             <div className={"hostArticle"}>
                 <div className={"hostContainer"}>
-                    <img className={"hostImage"} src={img} alt=""/>
+                    <ProfilePicture img={carData.host.user.imageSource}/>
                     <div className={"vText"}><GiRoundStar />{carData.host.user.name}</div>
                 </div>
                 <div className="swiperContainer">
