2c29918e62cee882e6db6f8c46f45def6ada1cba
zsollti <zsol.pz@gmail.com>
2022-09-05 23:55:23 +0200

    find carfeedbacks for car through api

--- carngo/src/main/java/com/codecool/carngo/service/CarFeedbackService.java
+++ carngo/src/main/java/com/codecool/carngo/service/CarFeedbackService.java
@@ -1,6 +1,7 @@
 package com.codecool.carngo.service;
 
 import com.codecool.carngo.model.CarFeedbackModel;
+import com.codecool.carngo.model.UserFeedbackModel;
 import com.codecool.carngo.repository.CarFeedbackRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,5 +20,9 @@ public class CarFeedbackService {
 
     public List<CarFeedbackModel> getAllCarFeedback(){
         return carFeedbackRepository.findAll();
+    }
+
+    public List<CarFeedbackModel> getCarFeedbacksForCar(Long id) {
+        return carFeedbackRepository.findCarFeedbacksForCar(id);
     }
 }
