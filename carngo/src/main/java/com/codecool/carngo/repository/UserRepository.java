561786b32d65d32f8a17f7351252d236dd7a701f
doni002 <alpardonat02@gmail.com>
2022-09-05 14:37:17 +0200

    add user login validation and password encryption

--- carngo/src/main/java/com/codecool/carngo/repository/UserRepository.java
+++ carngo/src/main/java/com/codecool/carngo/repository/UserRepository.java
@@ -1,13 +1,20 @@
 package com.codecool.carngo.repository;
 
 import com.codecool.carngo.model.UserModel;
+import com.codecool.carngo.model.VehicleModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 
 import javax.transaction.Transactional;
+import java.util.List;
 
 public interface UserRepository extends JpaRepository<UserModel, Long> {
 
+    @Query(nativeQuery = true, value = "SELECT * FROM users WHERE name = ?1")
+    List<UserModel> getUsersByName(String name);
+
+    @Query(nativeQuery = true, value = "SELECT * FROM users WHERE email = ?1")
+    List<UserModel> getUsersByEmail(String email);
 
 }
