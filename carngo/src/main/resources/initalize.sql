eec7fbf78e9cae30f137ea2ed0a4234694b92b90
zsollti <zsol.pz@gmail.com>
2022-09-05 21:25:49 +0200

    extends initial db data

--- carngo/src/main/resources/initalize.sql
+++ carngo/src/main/resources/initalize.sql
@@ -106,17 +106,23 @@ VALUES (1, 'Tom B. Erichsen', 'Erichsen@gmail.com', 'Password123', 'defaultProfi
 INSERT INTO users (id, name, email, password, image_source)
 VALUES (2, 'Ashlynn', 'Ashlynn@gmail.com', '987654321', 'defaultProfile.jpg');
 
+INSERT INTO users (id, name, email, password, image_source)
+VALUES (3, 'Brandon', 'brandon@gmail.com', '987651321', 'defaultProfile.jpg');
+
 INSERT INTO hosts (id, bank_account_number, bank_account_name, user_id)
-VALUES (2, 404307469, 'Erichsen', 1);
+VALUES (1, 404307469, 'Erichsen', 1);
+
+INSERT INTO hosts (id, bank_account_number, bank_account_name, user_id)
+VALUES (2, 404307462, 'Brandon', 3);
 
 INSERT INTO vehicles (id, description, car_type, color, brand, model, fuel, image_source, vintage, num_of_seats, trunk_capacity, price_per_day, num_of_reservations, owner_id)
-VALUES (100, 'noi tulajdonostol alig hasznalt', 'sedan', 'grey', 'Toyota', 'Tacoma','hybrid', 'travis-essingerSquare.jpg', 2020, 5, 500, 79, 3, 2);
+VALUES (100, 'noi tulajdonostol alig hasznalt', 'sedan', 'grey', 'Toyota', 'Tacoma','hybrid', 'travis-essingerSquare.jpg', 2020, 5, 500, 79, 3, 1);
 
 INSERT INTO vehicles (id, description, car_type, color, brand, model, fuel, image_source, vintage, num_of_seats, trunk_capacity, price_per_day, num_of_reservations, owner_id)
-VALUES (200, 'noi tulajdonostol alig hasznalt', 'sedan', 'gold', 'Audi', 'RS','petrol', 'Audi-RSSquare.jpg', 2021, 5, 300, 121, 4, 2);
+VALUES (200, 'noi tulajdonostol alig hasznalt', 'sedan', 'gold', 'Audi', 'RS','petrol', 'Audi-RSSquare.jpg', 2021, 5, 300, 121, 4, 1);
 
 INSERT INTO vehicles (id, description, car_type, color, brand, model, fuel, image_source, vintage, num_of_seats, trunk_capacity, price_per_day, num_of_reservations, owner_id)
-VALUES (300, 'noi tulajdonostol alig hasznalt', 'sedan', 'red', 'Ferrari', 'F40','petrol', 'ferrariSquare.jpg', 2004, 5, 100, 160, 5, 2);
+VALUES (300, 'noi tulajdonostol alig hasznalt', 'sedan', 'red', 'Ferrari', 'F40','petrol', 'ferrariSquare.jpg', 2004, 5, 100, 160, 5, 1);
 
 INSERT INTO vehicles (id, description, car_type, color, brand, model, fuel, image_source, vintage, num_of_seats, trunk_capacity, price_per_day, num_of_reservations, owner_id)
 VALUES (400, 'noi tulajdonostol alig hasznalt', 'sedan', 'grey', 'Bugatti', 'Chiron','petrol', 'bugattiSquare.jpeg', 2022, 5, 100, 205, 5, 2);
@@ -138,5 +144,13 @@ VALUES (1, 3.9, 'i like the ride', 2, '2022-09-28',100);
 
 /*that's actually a host feedback*/
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
-VALUES (1, 3.9, 'the host was really kind', 2, 2, '2022-09-28');
+VALUES (1, 3.9, 'the host was really kind', 1, 2, '2022-09-28');
 
+INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
+VALUES (2, 2.8, 'He was drunk as hell.', 2, 2, '2022-09-24');
+
+INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
+VALUES (3, 4.2, 'This car is awesome!!', 2, 1, '2022-09-22');
+
+INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
+VALUES (4, 4.8, 'Hell yeah, baby!!!', 3, 1, '2022-09-21');
