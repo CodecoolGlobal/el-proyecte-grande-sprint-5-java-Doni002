1b7ecfded510b2571c32f718bdff320a41dcb817
zsollti <zsol.pz@gmail.com>
2022-09-06 00:02:09 +0200

    refactor vehicle sliders

--- carngo/src/main/webapp/aurora/src/components/vehicle/VehicleSlider.js
+++ carngo/src/main/webapp/aurora/src/components/vehicle/VehicleSlider.js
@@ -3,10 +3,58 @@ import { useState } from 'react'
 import './vehicleDetail.css';
 import 'swiper/css/navigation';
 import { Navigation, Thumbs } from 'swiper'
+import imgOldTimer from "../img/oldtimerSquare.jpg";
+import imgExtreme from "../img/travis-essingerSquare.jpg";
+import imgMustang from "../img/mustangSquare.jpg";
+import imgBeast from "../img/beastSquare.jpg";
+import imgAudiWater from "../img/audiwaterSquare.jpg";
+import imgAudiRs from "../img/Audi-RSSquare.jpg";
+import imgTransport from "../img/mostafa-tarekSquare.jpg";
+import {useEffect} from "react";
 
 const VehicleSlider = (props) => {
-    const images = props.images;
+    const images = [
+        {
+            imgSrc: imgOldTimer,
+        },
+        {
+            imgSrc: imgExtreme,
+        },
+        {
+            imgSrc: imgMustang,
+        },
+        {
+            imgSrc: imgBeast,
+        },
+        {
+            imgSrc: imgAudiWater,
+        },
+        {
+            imgSrc: imgAudiRs,
+        },
+        {
+            imgSrc: imgTransport,
+        }
+    ];
+
+    const carData = props.carData;
     const [activeThumb, setActiveThumb] = useState();
+    const [img, setImg] = useState([]);
+
+    useEffect(() => {
+        const fetchImage = async () => {
+            const response = await fetch(`http://localhost:8080/api/image/carProfile/${carData.imageSource}`);
+            const imageBlob = await response.blob();
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+            setImg([imageObjectURL, imageObjectURL]);
+        };
+
+        fetchImage().then();
+    }, [carData]);
+
+    if(img === undefined){
+        return (<h1>Loading...</h1>)
+    }
 
     return (<>
             <div className={"swiper-slide-container"}>
