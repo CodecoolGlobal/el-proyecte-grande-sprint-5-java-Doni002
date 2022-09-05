2c48d3646405c678f53e76962a810d4d47c54a46
zsollti <zsol.pz@gmail.com>
2022-09-05 23:56:18 +0200

    feature: profile picture component with fetch

---
+++ carngo/src/main/webapp/aurora/src/components/profilePicture/ProfilePicture.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+import defaultPicture from "../img/defaultProfile.jpg";
+
+function Profile(props){
+    const path = props.img;
+    const [img, setImg] = useState(undefined);
+    useEffect(() => {
+        const fetchImage = async () => {
+            const response = await fetch(`http://localhost:8080/api/image/userProfile/${path}`);
+            const imageBlob = await response.blob();
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+            setImg(imageObjectURL);
+        };
+
+        fetchImage().then();
+    }, [props.img, path]);
+
+    if(img === undefined || path === undefined){
+        return (<img className={"hostImage"} src={defaultPicture} alt=""/>)
+    }
+    return(<img className={"hostImage"} src={img} alt=""/>)
+}
+export default Profile;
