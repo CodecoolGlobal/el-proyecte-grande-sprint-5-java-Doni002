5b49a5e1338e553c8441d67209b7dbc480262a88
zsollti <zsol.pz@gmail.com>
2022-09-05 23:57:42 +0200

    refactor vehicle details

--- carngo/src/main/webapp/aurora/src/pages/VehicleDetail.js
+++ carngo/src/main/webapp/aurora/src/pages/VehicleDetail.js
@@ -4,13 +4,6 @@ import VehicleSlider from "../components/vehicle/VehicleSlider";
 import VehicleFeatures from "../components/vehicle/VehicleFeatures";
 import HostReview from "../components/vehicle/HostReview";
 import VehicleReviewSlider from "../components/vehicle/VehicleReviewSlider";
-import imgOldTimer from "../components/img/oldtimerSquare.jpg";
-import imgExtreme from "../components/img/travis-essingerSquare.jpg";
-import imgMustang from "../components/img/mustangSquare.jpg";
-import imgBeast from "../components/img/beastSquare.jpg";
-import imgAudiWater from "../components/img/audiwaterSquare.jpg";
-import imgAudiRs from "../components/img/Audi-RSSquare.jpg";
-import imgTransport from "../components/img/mostafa-tarekSquare.jpg";
 import Footer from "../components/footer/Footer";
 import {Parallax} from "react-parallax";
 import Navbar from "../components/header/Navbar";
@@ -50,77 +43,21 @@ const VehicleDetail = () => {
         getData().then();
     }, [error]);
 
-    const images = [
-        {
-            imgSrc: imgOldTimer,
-        },
-        {
-            imgSrc: imgExtreme,
-        },
-        {
-            imgSrc: imgMustang,
-        },
-        {
-            imgSrc: imgBeast,
-        },
-        {
-            imgSrc: imgAudiWater,
-        },
-        {
-            imgSrc: imgAudiRs,
-        },
-        {
-            imgSrc: imgTransport,
-        }
-    ]
-    const hostReviews = [
-        {name: "David",
-        review: "Good car and host."},
-        {name: "Tracey",
-            review: "Precise desciption."},
-        {name: "Joey",
-            review: "Had fun."},
-        {name: "Bud",
-            review: "Didn't meet my expectations."}
-    ]
-    const vehicleReviews = [
-        {name: "Claire",
-        image: imgExtreme,
-        review: "Good car and host.",
-        experience: 4,
-        condition:5,
-        consumption:5},
-        {name: "Brandon",
-            image: imgAudiWater,
-        review: "Precise desciption.",
-        experience: 4,
-        condition:5,
-        consumption:5},
-        {name: "Lucy",
-            image: imgTransport,
-        review: "Had fun.",
-        experience: 4,
-        condition:5,
-        consumption:5},
-        {name: "George",
-            image: imgBeast,
-        review: "Didn't meet my expectations.",
-        experience: 4,
-        condition:5,
-        consumption:5}
-    ]
+    if(data === undefined){
+        return(<h1>Please wait 1 second</h1>)
+    }
     return (
         <>
             <Parallax bgImage={imgAbstract} strength={100}>
                 <Navbar />
             </Parallax>
-            <VehicleSlider images={ images}/>
+            <VehicleSlider carData={ data}/>
             <div className="vehicleContainer">
                 <VehicleFeatures data={data} />
                 <CalendarArticle />
             </div>
             <HostReview data={data}/>
-            <VehicleReviewSlider reviews={ vehicleReviews}/>
+            <VehicleReviewSlider carData={data}/>
             <Footer />
         </>
     );
