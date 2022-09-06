83c41744cb66d5fff19da95787bf14e7f6827f08
zsollti <zsol.pz@gmail.com>
2022-09-06 12:26:46 +0200

    fetch vehicles slider data through api

--- carngo/src/main/webapp/aurora/src/components/vehicle/VehicleSlider.js
+++ carngo/src/main/webapp/aurora/src/components/vehicle/VehicleSlider.js
@@ -1,31 +1,99 @@
 import { Swiper, SwiperSlide } from 'swiper/react';
-import { Navigation, Thumbs } from 'swiper'
 import { useState } from 'react'
 
 import './vehicleDetail.css';
 import 'swiper/css/navigation';
-
+import { Navigation, Thumbs } from 'swiper'
+import defaultPicture from "../img/defaultProfile.jpg";
+import {useEffect} from "react";
+import CarPicture from "../imageFetch/CarPicture";
 
 const VehicleSlider = (props) => {
     const carData = props.carData;
     const [activeThumb, setActiveThumb] = useState();
-    const [img, setImg] = useState([]);
+    const [data, setData] = useState(undefined);
 
     useEffect(() => {
-        const fetchImage = async () => {
-            const response = await fetch(`http://localhost:8080/api/image/carProfile/${carData.imageSource}`);
-            const imageBlob = await response.blob();
-            const imageObjectURL = URL.createObjectURL(imageBlob);
-            setImg([imageObjectURL, imageObjectURL]);
-        };
+        const getData = async () => {
+            const response = await fetch(
+                `http://localhost:8080/api/car-images/${carData.id}`
+            );
+            if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            }
+            let actualData = await response.json();
+            setData(actualData);
+        }
+        getData().catch((response)=>{console.log(response);});
+    }, [carData, props.carData]);
+    if(data===undefined){
+        return (<>
+                <div className={"swiper-slide-container"}>
+                    <div className={"swiper-slide"}>
 
-        fetchImage().then();
-    }, [carData]);
+                        <Swiper
+                            loop={true}
+                            spaceBetween={10}
+                            navigation={true}
+                            modules={[Navigation, Thumbs]}
+                            grabCursor={false}
+                            thumbs={{ swiper: activeThumb }}
+                            className={"product-images-slider"}
+                        >
 
-    if(img === undefined){
-        return (<h1>Loading...</h1>)
+                            <SwiperSlide className={"default-item"}>
+                                <img src={defaultPicture} alt="" />
+                            </SwiperSlide>
+
+                        </Swiper>
+                        <Swiper
+                            loop={false}
+                            spaceBetween={20}
+                            slidesPerView={8}
+                            modules={[Navigation, Thumbs]}
+                            grabCursor={true}
+                            className={"product-images-slider-thumbs border-bottom"}
+                            onSwiper={setActiveThumb}
+                            breakpoints={{
+                                0: {
+                                    slidesPerView:1,
+                                    spaceBetween: 10,
+                                },
+                                630: {
+                                    slidesPerView:2,
+                                    spaceBetween: 15,
+                                },
+                                880: {
+                                    slidesPerView:3,
+                                    spaceBetween: 15,
+                                },
+                                1150: {
+                                    slidesPerView: 4,
+                                    spaceBetween: 20,
+                                },
+                                1450: {
+                                    slidesPerView: 5,
+                                    spaceBetween: 20,
+                                },
+                                1730: {
+                                    slidesPerView: 6,
+                                    spaceBetween: 30,
+                                }
+                            }}
+                        >
+                                <SwiperSlide>
+                                    <div className="product-images-slider-thumbs-item">
+                                        <img src={defaultPicture} alt="" />
+                                    </div>
+                                </SwiperSlide>
+                        </Swiper>
+                    </div>
+                </div>
+            </>
+        );
     }
-
     return (<>
             <div className={"swiper-slide-container"}>
                 <div className={"swiper-slide"}>
@@ -40,9 +108,9 @@ const VehicleSlider = (props) => {
             className={"product-images-slider"}
         >
             {
-                images.map((item, index) => (
+                data.map((item, index) => (
                     <SwiperSlide key={index} className={"default-item"}>
-                        <img src={item.imgSrc} alt="" />
+                        <CarPicture img={item.imageSource}/>
                     </SwiperSlide>
                 ))
             }
@@ -83,10 +151,10 @@ const VehicleSlider = (props) => {
                     }}
                 >
             {
-                images.map((item, index) => (
+                data.map((item, index) => (
                     <SwiperSlide key={index}>
                         <div className="product-images-slider-thumbs-item">
-                            <img src={item.imgSrc} alt="" />
+                            <CarPicture img={item.imageSource}/>
                         </div>
                     </SwiperSlide>
                 ))
