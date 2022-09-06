19c463dd55704e291deffa1e8084800e1e767380
zsollti <zsol.pz@gmail.com>
2022-09-06 12:24:28 +0200

    car picture component with fetch

---
+++ carngo/src/main/webapp/aurora/src/components/imageFetch/CarPicture.js
@@ -0,0 +1,23 @@
+import {useEffect, useState} from "react";
+import defaultPicture from "../img/defaultProfile.jpg";
+
+function CarPicture(props){
+    const path = props.img;
+    const [img, setImg] = useState(undefined);
+    useEffect(() => {
+        const fetchImage = async () => {
+            const response = await fetch(`http://localhost:8080/api/image/carProfile/${path}`);
+            const imageBlob = await response.blob();
+            const imageObjectURL = URL.createObjectURL(imageBlob);
+            setImg(imageObjectURL);
+        };
+
+        fetchImage().then();
+    }, [props.img, path]);
+
+    if(img === undefined || path === undefined){
+        return (<img src={defaultPicture} alt=""/>)
+    }
+    return(<img src={img} alt=""/>)
+}
+export default CarPicture;
