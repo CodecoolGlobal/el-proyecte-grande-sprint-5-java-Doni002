3055b85b87ce2e4904cba69abe9fde9469d6832f
zsollti <zsol.pz@gmail.com>
2022-09-07 12:52:59 +0200

    extend initial db data

--- carngo/src/main/resources/initalize.sql
+++ carngo/src/main/resources/initalize.sql
@@ -13,6 +13,8 @@ CREATE TABLE users (
                        email varchar(255),
                        password varchar(255),
                        image_source varchar(255),
+                       profile_description varchar(511),
+                       number_of_cars int,
                        PRIMARY KEY (id)
 );
 
@@ -50,9 +52,8 @@ CREATE TABLE vehicles (
 CREATE TABLE car_availability (
                       id int,
                       vehicle_id int,
-                      "from" date,
-                      "to" date,
-                      status boolean,
+                      from_date date,
+                      to_date date,
                       PRIMARY KEY (id),
                       FOREIGN KEY (vehicle_id)
                           REFERENCES vehicles(id)
@@ -60,8 +61,8 @@ CREATE TABLE car_availability (
 
 CREATE TABLE car_reservation (
                      id int,
-                     "from" date,
-                     "to" date,
+                     from_date date,
+                     to_date date,
                      renter_user_id int,
                      vehicle_id int,
                      PRIMARY KEY (id),
@@ -111,26 +112,26 @@ CREATE TABLE car_images (
 
 
 /* TEST DATA */
-INSERT INTO users (id, name, email, password, image_source)
-VALUES (7, 'Donát Alpár', 'alpardonat02@gmail.com', '1234', 'AlparDonat.jpg');
+INSERT INTO users (id, name, email, password, image_source, profile_description, number_of_cars)
+VALUES (7, 'Donát Alpár', 'alpardonat02@gmail.com', '1234', 'AlparDonat.jpg', 'A Young And Enthusiastic Software Developer With Critical Thinking, Problem-Solving, Negotiation, Outstanding In Both Oral And Written Communication, And Easily Adaptable Interpersonal Skills. Moreover, Working In A Team Means No Difficulties Due To My Emotional Intelligence And Social Collaboration, Also Rather An Exciting Journey For Me.', 1);
 
-INSERT INTO users (id, name, email, password, image_source)
-VALUES (6, 'Barbara Huszár', 'szabo.huszar.barbara@gmail.com', '1234', 'BarbaraHuszar.jpg');
+INSERT INTO users (id, name, email, password, image_source, profile_description, number_of_cars)
+VALUES (6, 'Barbara Huszár', 'szabo.huszar.barbara@gmail.com', '1234', 'BarbaraHuszar.jpg', 'Solution-Focused Mind With A Creative Touch. Eager To Learn. Her Easy Going Personality Makes It Easy To Make A Good Fit In Any Team.', 1);
 
-INSERT INTO users (id, name, email, password, image_source)
-VALUES (5, 'Roland Márton', 'roland111187@gmail.com', '1234', 'RolandMarton.jpeg');
+INSERT INTO users (id, name, email, password, image_source, profile_description, number_of_cars)
+VALUES (5, 'Roland Márton', 'roland111187@gmail.com', '1234', 'RolandMarton.jpeg', 'A Young And Enthusiastic Software Developer With Critical Thinking, Problem-Solving, Negotiation, Outstanding In Both Oral And Written Communication, And Easily Adaptable Interpersonal Skills. Moreover, Working In A Team Means No Difficulties Due To My Emotional Intelligence And Social Collaboration, Also Rather An Exciting Journey For Me. I''m Still Enthusiastic About Grabbing Onto Any Other Programming Languages, Frameworks, And Principles I Can Integrate Into The Coding Web In My Head.', 1);
 
-INSERT INTO users (id, name, email, password, image_source)
-VALUES (4, 'Zsolt Pintér', 'zsol.pz@gmail.com', '1234', 'ZsoltPinter.jpg');
+INSERT INTO users (id, name, email, password, image_source, profile_description, number_of_cars)
+VALUES (4, 'Zsolt Pintér', 'zsol.pz@gmail.com', '1234', 'ZsoltPinter.jpg', 'An Ambitious And Aspiring Fullstack Software Developer From Codecool Academy With Excellent Hard Skills To Work On The Most Difficult Projects, Problems. Moreover, Distinguished Soft Capabilities To Work In A Team As A Leader Or Member Either. I''m Still Enthusiastic About Grabbing Onto Any Other Programming Languages, Frameworks.', 1);
 
-INSERT INTO users (id, name, email, password, image_source)
-VALUES (1, 'Tom B. Erichsen', 'Erichsen@gmail.com', 'Password123', 'defaultProfile.jpg');
+INSERT INTO users (id, name, email, password, image_source, profile_description, number_of_cars)
+VALUES (1, 'Tom B. Erichsen', 'Erichsen@gmail.com', 'Password123', 'defaultProfile.jpg','This profile has not got any description.', 2);
 
-INSERT INTO users (id, name, email, password, image_source)
-VALUES (2, 'Ashlynn', 'Ashlynn@gmail.com', '987654321', 'defaultProfile.jpg');
+INSERT INTO users (id, name, email, password, image_source, profile_description, number_of_cars)
+VALUES (2, 'Ashlynn', 'Ashlynn@gmail.com', '987654321', 'defaultProfile.jpg', 'This profile has not got any description.', 0);
 
-INSERT INTO users (id, name, email, password, image_source)
-VALUES (3, 'Brandon', 'brandon@gmail.com', '987651321', 'defaultProfile.jpg');
+INSERT INTO users (id, name, email, password, image_source, profile_description, number_of_cars)
+VALUES (3, 'Brandon', 'brandon@gmail.com', '987651321', 'defaultProfile.jpg', 'This profile has not got any description.', 1);
 
 INSERT INTO hosts (id, bank_account_number, bank_account_name, user_id)
 VALUES (1, 404307469, 'Erichsen', 1);
@@ -168,10 +169,10 @@ VALUES (500, 'Do not even look at it.', 'sedan', 'yellow', 'Mercedes', 'AMG GT',
 INSERT INTO vehicles (id, description, car_type, color, brand, model, fuel, image_source, vintage, num_of_seats, trunk_capacity, price_per_day, num_of_reservations, owner_id)
 VALUES (600, 'It is grey. I think.', 'sedan', 'blue', 'McLaren', '720S','petrol', 'mclarenSquare.jpg', 2021, 5, 200, 210, 5, 5);
 
-INSERT INTO car_availability (id, vehicle_id, "from", "to")
+INSERT INTO car_availability (id, vehicle_id, from_date, to_date)
 VALUES (10, 100, '2022-09-30', '2022-10-01');
 
-INSERT INTO car_reservation (id, "from", "to", renter_user_id, vehicle_id)
+INSERT INTO car_reservation (id, from_date, to_date, renter_user_id, vehicle_id)
 VALUES (1, '2022-09-27', '2022-09-28', 2,100);
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
@@ -190,7 +191,7 @@ INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (5, 5, 'Precise, trustworthy, excellent!', 5, 6, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
-VALUES (6, 5, 'I kinda like him :)', 6, 6, '2022-09-21');
+VALUES (6, 5, 'It was fun', 6, 6, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (7, 5, 'He has the best car ever!', 4, 6, '2022-09-21');
@@ -199,7 +200,7 @@ INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (8, 5, 'Precise, trustworthy, excellent!', 7, 5, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
-VALUES (9, 5, 'I kinda like him :)', 6, 5, '2022-09-21');
+VALUES (9, 5, 'It was fun', 6, 5, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (10, 5, 'He has the best car ever!', 4, 5, '2022-09-21');
@@ -208,7 +209,7 @@ INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (11, 5, 'Precise, trustworthy, excellent!', 7, 4, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
-VALUES (12, 5, 'I kinda like him :)', 6, 4, '2022-09-21');
+VALUES (12, 5, 'It was fun', 6, 4, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (13, 5, 'He has the best car ever!', 5, 4, '2022-09-21');
@@ -217,13 +218,13 @@ INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (14, 5, 'Precise, trustworthy, excellent!', 7, 3, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
-VALUES (15, 5, 'I kinda like him :)', 5, 3, '2022-09-21');
+VALUES (15, 5, 'It was fun', 5, 3, '2022-09-21');
 
 INSERT INTO user_feedback (id, star_rating, message, user_id, host_id, date)
 VALUES (16, 5, 'He has the best car ever!', 4, 3, '2022-09-21');
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (1, 3.9, 'Precise desciption.', 7, '2022-09-28',100);
+VALUES (1, 3.9, 'Precise description.', 7, '2022-09-28',100);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (2, 3.9, 'Had fun.', 6, '2022-09-28',100);
@@ -235,7 +236,7 @@ INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (4, 3.9, 'Good car and host', 4, '2022-09-28',100);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (5, 3.9, 'Precise desciption.', 7, '2022-09-28',200);
+VALUES (5, 3.9, 'Precise description.', 7, '2022-09-28',200);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (6, 3.9, 'Had fun.', 6, '2022-09-28',200);
@@ -247,10 +248,10 @@ INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (8, 3.9, 'Good car and host', 4, '2022-09-28',200);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (9, 3.9, 'Precise desciption.', 7, '2022-09-28',300);
+VALUES (9, 3.9, 'Precise description.', 7, '2022-09-28',300);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (10, 3.9, 'Had fun.', 6, '2022-09-28',300);
+VALUES (10, 3.9, 'Had fun.', 1, '2022-09-28',300);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (11, 3.9, 'Did not meet my expectations.', 5, '2022-09-28',300);
@@ -259,7 +260,7 @@ INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (12, 3.9, 'Good car and host', 4, '2022-09-28',300);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (13, 3.9, 'Precise desciption.', 7, '2022-09-28',400);
+VALUES (13, 3.9, 'Precise description.', 2, '2022-09-28',400);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (14, 3.9, 'Had fun.', 6, '2022-09-28',400);
@@ -271,7 +272,7 @@ INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (16, 3.9, 'Good car and host', 4, '2022-09-28',400);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (17, 3.9, 'Precise desciption.', 7, '2022-09-28',500);
+VALUES (17, 3.9, 'Precise description.', 7, '2022-09-28',500);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (18, 3.9, 'Had fun.', 6, '2022-09-28',500);
@@ -280,16 +281,16 @@ INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (19, 3.9, 'Did not meet my expectations.', 5, '2022-09-28',500);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (20, 3.9, 'Good car and host', 4, '2022-09-28',500);
+VALUES (20, 3.9, 'Good car and host', 3, '2022-09-28',500);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (21, 3.9, 'Precise desciption.', 7, '2022-09-28',600);
+VALUES (21, 3.9, 'Precise description.', 7, '2022-09-28',600);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (22, 3.9, 'Had fun.', 6, '2022-09-28',600);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
-VALUES (23, 3.9, 'Did not meet my expectations.', 5, '2022-09-28',600);
+VALUES (23, 3.9, 'Did not meet my expectations.', 1, '2022-09-28',600);
 
 INSERT INTO car_feedback (id, star_rating, message, user_id, date, vehicle_id)
 VALUES (24, 3.9, 'Good car and host', 4, '2022-09-28',600);
