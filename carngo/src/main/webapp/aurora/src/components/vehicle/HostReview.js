d92c46d94398e96a89c5a48c4ca54365f9defcde
zsollti <zsol.pz@gmail.com>
2022-09-07 13:22:00 +0200

    make links top profiles

--- carngo/src/main/webapp/aurora/src/components/vehicle/HostReview.js
+++ carngo/src/main/webapp/aurora/src/components/vehicle/HostReview.js
@@ -3,6 +3,7 @@ import "./vehicleDetail.css"
 import {GiRoundStar} from 'react-icons/gi';
 import HostReviewSlider from "./HostReviewSlider";
 import ProfilePicture from "../imageFetch/ProfilePicture";
+import {Link} from "react-router-dom";
 
 function HostReview(props) {
     const carData = props.data;
@@ -10,8 +11,11 @@ function HostReview(props) {
         <>
             <div className={"host-h3"}>HOSTED BY</div>
             <div className={"hostArticle"}>
+
                 <div className={"hostContainer"}>
-                    <ProfilePicture img={carData.host.user.imageSource}/>
+                    <Link to={"/profile/" + carData.host.user.id}>
+                        <ProfilePicture img={carData.host.user.imageSource}/>
+                    </Link>
                     <div className={"vText"}><GiRoundStar />{carData.host.user.name}</div>
                 </div>
                 <div className="swiperContainer">
