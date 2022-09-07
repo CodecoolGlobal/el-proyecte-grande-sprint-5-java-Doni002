7610705e87f995ac4596534248b44fc3b4269461
zsollti <zsol.pz@gmail.com>
2022-09-07 12:55:54 +0200

    get car availability data through api

--- carngo/src/main/webapp/aurora/src/components/vehicle/CalendarArticle.js
+++ carngo/src/main/webapp/aurora/src/components/vehicle/CalendarArticle.js
@@ -2,10 +2,31 @@ import React, { useState } from 'react';
 import Calendar from 'react-calendar';
 import "./vehicleDetail.css"
 import 'react-calendar/dist/Calendar.css';
+import {useEffect} from "react";
+import {SwiperSlide} from "swiper/react";
 
 
 function CalendarArticle(props) {
+    const car = props.data;
     const [date, setDate] = useState(new Date());
+    const [data, setData] = useState(undefined);
+
+    useEffect(() => {
+        const getData = async () => {
+            const response = await fetch(
+                `http://localhost:8080/api/car-availability/${car.id}`
+            );
+            if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            }
+            let actualData = await response.json();
+            setData(actualData);
+        }
+        getData().catch(reason => {console.log(reason)});
+    }, );
+
     return (
         <>
             <div className={"vArticle"}>
