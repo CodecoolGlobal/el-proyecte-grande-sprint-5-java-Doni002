a961bf1b682a66b92b7d500ac15923563886078e
zsollti <zsol.pz@gmail.com>
2022-09-07 12:54:20 +0200

    extend user model, hotfix user controller route

--- carngo/src/main/java/com/codecool/carngo/controller/UserController.java
+++ carngo/src/main/java/com/codecool/carngo/controller/UserController.java
@@ -29,7 +29,7 @@ public class UserController {
         return new ResponseEntity<>(userService.getAllUser(), HttpStatus.OK);
     }
 
-    @GetMapping(value = "{id}")
+    @GetMapping(value = "/{id}")
     public ResponseEntity<Optional<UserModel>> getUserById(@PathVariable("id") Long id){
         Optional<UserModel> user = userService.getUserById(id);
         if(user.isPresent()){
@@ -38,7 +38,7 @@ public class UserController {
         return new ResponseEntity<>(HttpStatus.NOT_FOUND);
     }
 
-    @PostMapping(value = "register")
+    @PostMapping(value = "/register")
     public ResponseEntity<String> registerUser(@RequestBody() Map<String, String> body){
         int response = userService.addUser(body);
         if(response == 200){
