106081fece335f0f5a619896d4a508f9ff8125be
doni002 <alpardonat02@gmail.com>
2022-09-08 11:59:39 +0200

    add user feedback apis

--- carngo/src/main/java/com/codecool/carngo/controller/UserFeedbackController.java
+++ carngo/src/main/java/com/codecool/carngo/controller/UserFeedbackController.java
@@ -6,12 +6,10 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+import java.util.Map;
 
 @RequestMapping("/api/user-feedback")
 @CrossOrigin(origins = "http://localhost:3000")
@@ -34,5 +32,34 @@ public class UserFeedbackController {
     @GetMapping(value="/{id}")
     public ResponseEntity<List<UserFeedbackModel>> getUserFeedbacksForHost(@PathVariable("id") Long id){
         return new ResponseEntity<>(userFeedbackService.getUserFeedbacksForHost(id), HttpStatus.OK);
+    }
+
+    //requirements: starRating, message, userId, hostId, date
+    @PostMapping
+    public ResponseEntity<String> addUserFeedbackModel(@RequestBody Map<String, String> body) {
+        int response = userFeedbackService.addUserFeedback(body);
+        if(response == 200) {
+            return new ResponseEntity<>("User feedback added successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("User or host not found!", HttpStatus.OK);
+    }
+
+    //requirements: id, starRating, message
+    @PutMapping
+    public ResponseEntity<String> editUserFeedbackModel(@RequestBody Map<String, String> body) {
+        int response = userFeedbackService.editUserFeedback(body);
+        if(response == 200) {
+            return new ResponseEntity<>("User feedback edited successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("User feedback not found with id: " + body.get("id"), HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "{id}")
+    public ResponseEntity<String> deleteUserFeedbackModel(@PathVariable Long id) {
+        int response = userFeedbackService.deleteUserFeedback(id);
+        if(response == 200) {
+            return new ResponseEntity<>("User feedback deleted successfully!", HttpStatus.OK);
+        }
+        return new ResponseEntity<>("User feedback not found with id: " + id, HttpStatus.OK);
     }
 }
