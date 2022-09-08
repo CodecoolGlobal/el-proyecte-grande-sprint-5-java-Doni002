32d4eeaa4413998af18241058c307f9d8b2f0a19
RolandMarton <roland111187@gmail.com>
2022-09-08 14:00:11 +0200

    Feature: Profile route redesigned with the database & Rework: Navbar and Profile

--- carngo/src/main/webapp/aurora/src/components/profile/CarGallery.js
+++ carngo/src/main/webapp/aurora/src/components/profile/CarGallery.js
@@ -6,33 +6,17 @@ import CarPicture from "../imageFetch/CarPicture";
 
 const CarGallery = (props) => {
     return (
-        <div className="gallery__column">
-            <div className="gallery__column">
+        <>
+            <div className="gallery__item">
                 <a href="#" className="gallery__link">
                     <figure className="gallery__thumb">
                         <CarPicture img={props.img} className={"gallery__image"}/>
+                        <figcaption className="gallery__caption">{props.brand} {props.model}</figcaption>
                     </figure>
                 </a>
             </div>
-        </div>
+        </>
     );
-    /*return (
-        <div className="gallery__column">
-            {props.collection.map(item=>{
-                return(
-                    <div className="gallery__column">
-                        <a href="#" className="gallery__link">
-                                <figure className="gallery__thumb">
-                                        <img src={item.imgSrc} alt={item.alt} className="gallery__image"/>
-                                        <figcaption className="gallery__caption">{item.alt}
-                                        </figcaption>
-                                </figure>
-                        </a>
-                    </div>
-                )
-            })}
-        </div>
-    );*/
 };
 
 export default CarGallery;
