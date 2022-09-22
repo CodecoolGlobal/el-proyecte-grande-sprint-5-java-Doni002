f25d65176e66ddf80b4ac1a87085ecae6b6010d3
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-09-22 15:13:40 +0200

    remove ProtectedRoutes

--- carngo/src/main/webapp/aurora/src/context/authContext.js
+++ carngo/src/main/webapp/aurora/src/context/authContext.js
@@ -14,16 +14,3 @@ export const AuthProvider = ({ userData, children }) => {
 
 export default AuthContext;
 
-/*
-export const ProtectedRoute = ({..rest }) => {
-    let { user } = useAuth();
-
-    if (!user || !user.token || user.token === "") {
-        return (
-            // component which inform the user that they must be logged in
-        );
-    }
-
-    // let user through if they're logged in
-    return <Route {..rest} />;
-};*/
