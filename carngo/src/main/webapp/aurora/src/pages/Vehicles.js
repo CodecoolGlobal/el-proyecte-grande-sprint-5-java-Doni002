4ac7a8c1c90aa49ab0d80c3104d19208fc646009
zsollti <zsol.pz@gmail.com>
2022-09-22 11:51:41 +0200

    car and host fetch at vehicles page

--- carngo/src/main/webapp/aurora/src/pages/Vehicles.js
+++ carngo/src/main/webapp/aurora/src/pages/Vehicles.js
@@ -8,12 +8,14 @@ import Navbar from "../components/header/Navbar";
 import CarSlider from "../components/car/CarSlider";
 import Footer from "../components/footer/Footer";
 import {useEffect, useState} from "react";
+import HostSlider from "../components/hosts/HostSlider";
 
 
 const Vehicles = () => {
     const [cars, setCars] = useState(undefined);
+    const [hosts, setHosts] = useState(undefined);
     useEffect(() => {
-        const getData = async () => {
+        const getCarData = async () => {
             const response = await fetch(
                 `http://localhost:8080/api/vehicles`
             );
@@ -25,17 +27,29 @@ const Vehicles = () => {
             let actualData = await response.json();
             setCars(actualData);
         }
-        getData().catch((response)=>{console.log(response);});
+        const getHostData = async () => {
+            const response = await fetch(
+                `http://localhost:8080/api/hosts`
+            );
+            if (!response.ok) {
+                throw new Error(
+                    `This is an HTTP error: The status is ${response.status}`
+                );
+            }
+            let actualData = await response.json();
+            setHosts(actualData);
+        }
+        getHostData().catch(reason => {console.log(reason)});
+        getCarData().catch((response)=>{console.log(response);});
 
-    }, []);
+    }, [cars, hosts]);
 
-    if(cars === undefined){
+    if(cars === undefined || hosts === undefined){
         return(
         <>
             <Parallax bgImage={CountryRoad} strength={-350}>
                 <Navbar />
             </Parallax>
-            <CarSlider/>
             <Footer />
         </>
         );
@@ -46,7 +60,8 @@ const Vehicles = () => {
                 <Navbar />
                 <DisplayMap cars={cars}/>
             </Parallax>
-            <CarSlider/>
+            <HostSlider hosts={hosts}/>
+            <CarSlider cars={cars}/>
             <Footer />
         </>
     );
