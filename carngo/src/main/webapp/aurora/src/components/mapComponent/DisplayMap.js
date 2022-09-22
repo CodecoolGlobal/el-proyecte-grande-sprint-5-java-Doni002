9ad00848e103a67cd6dd5b72f0f3f78888bc44a2
zsollti <zsol.pz@gmail.com>
2022-09-22 12:59:11 +0200

    DEBUG reload map when page reloaded, set map center to car

--- carngo/src/main/webapp/aurora/src/components/mapComponent/DisplayMap.js
+++ carngo/src/main/webapp/aurora/src/components/mapComponent/DisplayMap.js
@@ -43,11 +43,14 @@ const DisplayMap = (props) => {
 
     const [showLayer1, setShowLayer1] = useState(true);
     const [showLayer2, setShowLayer2] = useState(true);
-    const [showMarker, setShowMarker] = useState(true);
+    const [showMarker, setShowMarker] = useState(false);
     const [features, setFeatures] = useState(undefined);
 
     useEffect(() => {
         setFeatures(addMarkers(cars));
+        setCenter([cars[0].longitude, cars[0].latitude]);
+        setShowMarker(!showMarker);
+        setShowMarker(!showMarker);
     }, [props.cars, routePath]);
 
 
@@ -55,67 +58,71 @@ const DisplayMap = (props) => {
         return(<></>);
     }
 
-
-    return (
-        <div className="mapContainer">
-            <Map center={fromLonLat(center)} zoom={zoom}>
-                <Layers>
-                    <TileLayer source={osm()} zIndex={0} />
-                    {showLayer1 && (
-                        <VectorLayer
-                            source={vector({
-                                features: new GeoJSON().readFeatures(geojsonObject, {
-                                    featureProjection: get("EPSG:3857"),
-                                }),
-                            })}
-                            style={FeatureStyles.MultiPolygon}
-                        />
-                    )}
-                    {showLayer2 && (
-                        <VectorLayer
-                            source={vector({
-                                features: new GeoJSON().readFeatures(geojsonObject2, {
-                                    featureProjection: get("EPSG:3857"),
-                                }),
-                            })}
-                            style={FeatureStyles.MultiPolygon}
-                        />
-                    )}
-                    {showMarker && <VectorLayer source={vector({ features })} />}
-                </Layers>
-                <Controls>
-                    <FullScreenControl />
-                </Controls>
-            </Map>
-            <div className="checkBoxContainer">
-                <div>
-                    <input
-                        type="checkbox"
-                        checked={showLayer1}
-                        onChange={(event) => setShowLayer1(event.target.checked)}
-                    />{" "}
-                    Budapest - Third District
-                </div>
-                <div>
-                    <input
-                        type="checkbox"
-                        checked={showLayer2}
-                        onChange={(event) => setShowLayer2(event.target.checked)}
-                    />{" "}
-                    Budapest - First District
-                </div>
-                <hr />
-                <div>
-                    <input
-                        type="checkbox"
-                        checked={showMarker}
-                        onChange={(event) => setShowMarker(event.target.checked)}
-                    />{" "}
-                    Show markers
+    function render(){
+        return (
+            <div className="mapContainer">
+                <Map center={fromLonLat(center)} zoom={zoom}>
+                    <Layers>
+                        <TileLayer source={osm()} zIndex={0} />
+                        {showLayer1 && (
+                            <VectorLayer
+                                source={vector({
+                                    features: new GeoJSON().readFeatures(geojsonObject, {
+                                        featureProjection: get("EPSG:3857"),
+                                    }),
+                                })}
+                                style={FeatureStyles.MultiPolygon}
+                            />
+                        )}
+                        {showLayer2 && (
+                            <VectorLayer
+                                source={vector({
+                                    features: new GeoJSON().readFeatures(geojsonObject2, {
+                                        featureProjection: get("EPSG:3857"),
+                                    }),
+                                })}
+                                style={FeatureStyles.MultiPolygon}
+                            />
+                        )}
+                        {showMarker && <VectorLayer source={vector({ features })} />}
+                    </Layers>
+                    <Controls>
+                        <FullScreenControl />
+                    </Controls>
+                </Map>
+                <div className="checkBoxContainer">
+                    <div>
+                        <input
+                            type="checkbox"
+                            checked={showLayer1}
+                            onChange={(event) => setShowLayer1(event.target.checked)}
+                        />{" "}
+                        Budapest - Third District
+                    </div>
+                    <div>
+                        <input
+                            type="checkbox"
+                            checked={showLayer2}
+                            onChange={(event) => setShowLayer2(event.target.checked)}
+                        />{" "}
+                        Budapest - First District
+                    </div>
+                    <hr />
+                    <div>
+                        <input
+                            type="checkbox"
+                            checked={showMarker}
+                            onChange={(event) => setShowMarker(event.target.checked)}
+                        />{" "}
+                        Show markers
+                    </div>
                 </div>
             </div>
-        </div>
-    );
+        );
+    }
+
+    return render();
+
 };
 
 export default DisplayMap;
