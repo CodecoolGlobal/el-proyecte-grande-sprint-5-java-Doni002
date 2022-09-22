cf8b5e7b9465c2af3887d82a213636b28868d2f1
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-09-22 16:14:53 +0200

    fix token to accessToken

--- carngo/src/main/java/com/codecool/carngo/security/JwtResponse.java
+++ carngo/src/main/java/com/codecool/carngo/security/JwtResponse.java
@@ -1,14 +1,14 @@
 package com.codecool.carngo.security;
 
-import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.Setter;
 
 import java.util.List;
+
 @Getter
 @Setter
 public class JwtResponse {
-  private String token;
+  private String accessToken;
   private String type = "Bearer";
   private Long id;
   private String username;
@@ -16,7 +16,7 @@ public class JwtResponse {
   private List<String> roles;
 
   public JwtResponse(String accessToken, Long id, String username, String email, List<String> roles) {
-    this.token = accessToken;
+    this.accessToken = accessToken;
     this.id = id;
     this.username = username;
     this.email = email;
