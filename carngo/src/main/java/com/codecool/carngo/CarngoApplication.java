df5b12b4a09e243008261f0db94ac71a4d95a461
doni002 <alpardonat02@gmail.com>
2022-09-22 14:26:24 +0200

    add some config to display pictures but not working yet

--- carngo/src/main/java/com/codecool/carngo/CarngoApplication.java
+++ carngo/src/main/java/com/codecool/carngo/CarngoApplication.java
@@ -2,11 +2,23 @@ package com.codecool.carngo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+
+import java.io.IOException;
 
 @SpringBootApplication
-public class CarngoApplication {
+public class CarngoApplication extends SpringBootServletInitializer {
 
-    public static void main(String[] args) {
+    public static String IMAGE_DIR;
+
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(CarngoApplication.class);
+    }
+
+    public static void main(String[] args) throws IOException {
+        IMAGE_DIR = "/home/doni002/Desktop/Advanced/week_5/el-proyecte-grande-sprint-5-java-Doni002/carngo/src/main/resources/img/carProfiles/";
         SpringApplication.run(CarngoApplication.class, args);
     }
 
