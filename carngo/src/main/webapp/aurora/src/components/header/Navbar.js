3081fc1dc8d65e8fb19c08f2b2c56739fc260f8e
szabohuszarbarbara <szabo.huszar.barbara@gmail.com>
2022-09-23 09:20:31 +0200

    add login to shareyourcar rouete when not logged in

--- carngo/src/main/webapp/aurora/src/components/header/Navbar.js
+++ carngo/src/main/webapp/aurora/src/components/header/Navbar.js
@@ -22,6 +22,9 @@ const Navbar = () => {
     const addRoute = () => {
         return (loggedIn ?  `/profile/${user.id}` : "#");
     }
+    const addRouteCar = () => {
+        return (loggedIn ?  "/shareyourcar" : "#");
+    }
 
     const renderSignUpModal = () => {
         if(!loggedIn){
@@ -36,7 +39,7 @@ const Navbar = () => {
             <nav>
                 <li className="logo"><Link to="/"><h1>Aurora</h1></Link></li>
                 <ul className='menu'>
-                    <li className="item"><Link to="/shareyourcar"> <strong>Share your car</strong> </Link></li>
+                    <li onClick={renderSignUpModal}className="item"><Link to={addRouteCar()}> <strong>Share your car</strong> </Link></li>
                     <li className="item"><Link to="/vehicles"> Vehicles </Link></li>
                     <li className="item"><Link to="/contact"> Contact us </Link></li>
                     <li onClick={renderSignUpModal} className="item"><Link to={addRoute()}>{loggedIn ? 'Profile' : 'Sign Up' }  </Link></li>
